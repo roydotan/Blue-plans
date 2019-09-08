0df84acd9c480e5fece84ae1999d9c5d2b8f9c9e
roydotan <roydo@post.bgu.ac.il>
2019-09-08 21:06:48 +0300

    Updates

---
+++ precache-manifest.535607ab43f18fd6210f37e581afce3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2b40d52c44b1b25d9a5c0072d078fc",
+    "url": "/Blue-plans/index.html"
+  },
+  {
+    "revision": "0ffc5edb77728ee94368",
+    "url": "/Blue-plans/static/css/main.374694d7.chunk.css"
+  },
+  {
+    "revision": "11bdb6bdeda690be396f",
+    "url": "/Blue-plans/static/js/2.5a992caa.chunk.js"
+  },
+  {
+    "revision": "0ffc5edb77728ee94368",
+    "url": "/Blue-plans/static/js/main.d63b0e20.chunk.js"
+  },
+  {
+    "revision": "6f4524b8279ac4fa4671",
+    "url": "/Blue-plans/static/js/runtime~main.0d4d358c.js"
+  },
+  {
+    "revision": "75592b9721f5c3be101ff007677489c6",
+    "url": "/Blue-plans/static/media/defphoto.75592b97.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Blue-plans/static/media/logo.5d5d9eef.svg"
+  }
+]);
